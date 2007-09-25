dfd27b1bc302c6ec5c7c9f23aa9a175d79921b83
tebeka <tebeka@af1601bc-f7e6-0310-971a-b541ccad1583>
2007-09-25 16:11:26 +0000

    new project

--- trunk/pyexe.py
+++
@@ -1,7 +0,0 @@
-'''Just print out the location of the python executable'''
-
-__author__ = "Miki Tebeka <miki.tebeka@zoran.com>"
-# $Id: //DVD-R/ADP/i60/dev/tools/pyexe.py#2 $
-
-from sys import executable
-print executable
